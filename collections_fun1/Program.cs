2fd78c1dc305e4325203b70d0473dcd6bcb3733b
JMonell <joar.monell@gmail.com>
2024-10-14 14:40:17 +0200

    m

---
+++ collections_fun1/Program.cs
@@ -0,0 +1,24 @@
+﻿// using Seido.Utilities.SeedGenerator;
+
+namespace collections_fun1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var r = new Random();
+
+        List<int> list = new List<int>();
+        for (int i = 0; i <50; i++){
+            list.Add(r.Next(0,100));
+        }
+        foreach (int i in list){
+            System.Console.Write($"{i, 10}");
+            if(i %10 == 0){
+                System.Console.WriteLine();
+            }
+            
+        }
+        Console.WriteLine("Hello, World!");
+    }
+}
